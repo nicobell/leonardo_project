a39db38fcf341af625301756f250731d69fe9e3f
Giulia <giu.nardo1@gmail.com>
2019-01-13 11:51:03 +0100

    Revert "Merge branch 'master' into interfaccia"
    This reverts commit a555ff844b4f6d7582b5ab9bf39ba04cac5a8633.

--- miniPlayerData.js
+++ miniPlayerData.js
@@ -1,15 +1,1 @@
-klynt.miniPlayerData = {
-    "thanksForWatchingWording": "Thanks for watching!",
-    "resumePlaybackWording": "Resume playback?",
-    "fullscreenInfoWording": "This program will launch in fullscreen",
-    "yesWording": "Yes",
-    "title": "leonardo_e_la_città",
-    "thumbnail": "Medias/Thumbnails/leonardo_sfondo.jpg.jpg",
-    "noWording": "No",
-    "analyticsKey": "",
-    "launchAppWording": "Then Launch Project",
-    "redirectToMobileApp": "never",
-    "downloadAppWording": "Download App",
-    "url": "",
-    "description": ""
-}
+klynt.miniPlayerData = {"url":"","fullscreenInfoWording":"This program will launch in fullscreen","thanksForWatchingWording":"Thanks for watching!","resumePlaybackWording":"Resume playback?","title":"leonardo_e_la_città","yesWording":"Yes","redirectToMobileApp":"never","noWording":"No","analyticsKey":"","thumbnail":"Medias/Thumbnails/leonardo_sfondo.jpg.jpg","launchAppWording":"Then Launch Project","downloadAppWording":"Download App","description":""}
