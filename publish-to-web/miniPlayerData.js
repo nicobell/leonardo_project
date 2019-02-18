4a8b024a66b2a9f907a2a3cab36192d04e00f4a4
willyx78 <46424471+willyx78@users.noreply.github.com>
2019-02-18 20:03:16 +0100

    Update all'export del sito

--- publish-to-web/miniPlayerData.js
+++ publish-to-web/miniPlayerData.js
@@ -1,15 +1,1 @@
-klynt.miniPlayerData = {
-    "thanksForWatchingWording": "Thanks for watching!",
-    "resumePlaybackWording": "Resume playback?",
-    "fullscreenInfoWording": "This program will launch in fullscreen",
-    "yesWording": "Yes",
-    "title": "leonardo_e_la_città",
-    "thumbnail": "Medias/Thumbnails/leonardo_sfondo.jpg.jpg",
-    "noWording": "No",
-    "analyticsKey": "",
-    "redirectToMobileApp": "never",
-    "launchAppWording": "Then Launch Project",
-    "downloadAppWording": "Download App",
-    "url": "",
-    "description": ""
-}
+klynt.miniPlayerData = {"launchAppWording":"Then Launch Project","fullscreenInfoWording":"This program will launch in fullscreen","thanksForWatchingWording":"Thanks for watching!","analyticsKey":"","title":"leonardo_e_la_città","yesWording":"Yes","redirectToMobileApp":"never","thumbnail":"Player/css/editor/img/mini-player.jpg","noWording":"No","resumePlaybackWording":"Resume playback?","url":"","downloadAppWording":"Download App","description":""}
